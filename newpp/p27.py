f2f0f71f217e5aac99495edff998e84c4bbf6c03
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-07-23 13:04:31 +0530

    test py

---
+++ newpp/p27.py
@@ -0,0 +1,5 @@
+from sub27.p27 import mul
+
+def handler(event, context):
+
+    return {"message": add(3,3)}
---
+++ newpp/p36.py
@@ -0,0 +1,5 @@
+from sub36.p36 import div
+
+def handler(event, context):
+   
+    return {"message": div(3,3)}
